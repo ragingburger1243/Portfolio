0ebce1378fefe44abdf931b967ab2606f7c71259
Ragingburger <ragingburger668@gmail.com>
2026-04-19 16:36:25 +0200

    0.4

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ function makeDraggable(element) {
 makeDraggable(document.getElementById('window'))
 makeDraggable(document.getElementById('aboutme-window'))
 makeDraggable(document.getElementById('contact-window'))
+makeDraggable(document.getElementById('spotify-window'))
 
 
 document.getElementById('aboutme-btn').addEventListener('click', function() {
@@ -78,4 +79,50 @@ document.getElementById('copy-email').addEventListener('click', function (){
     navigator.clipboard.writeText('ragingburger668@gmail.com')
     this.textContent = 'Copied!'
     setTimeout(() => this.textContent = 'Copy', 2000)
+})
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        const screen = document.getElementById('loading-screen')
+        screen.style.transition = 'transform 0.8s cubic-bezier(0.76, 0, 0.24, 1)'
+        screen.style.transform = 'translateY(-100%)'
+    }, 2000)
+})
+let dots = ""
+setInterval(function (){
+    const text = document.getElementById('loading-text')
+
+    if (dots.length >= 3){
+        dots = ""
+    }else{
+        dots += "."
+        text.innerHTML = "Booting Burger OS" + dots
+    }
+
+}, 100)
+
+document.getElementById('spotify-btn').addEventListener('click', function (){
+    const audioplayer = document.getElementById('myAudio')
+    const icon = document.getElementById('play-icon')
+    audioplayer.volume = 0.3;
+
+    const modal = document.getElementById('spotify-window')
+    document.getElementById('welcome-text').style.opacity = '0'
+
+
+    if (modal.classList.contains('show')) {
+        modal.classList.remove('show')
+    } else {
+        modal.classList.add('show')
+    }
+    document.getElementById('play-btn').addEventListener('click', function (){
+        if (audioplayer.classList.contains('playing')) {
+            audioplayer.classList.remove('playing')
+            audioplayer.pause();
+            icon.textContent = '▶'
+        } else {
+            audioplayer.classList.add('playing')
+            audioplayer.play();
+            icon.textContent = '⏸'
+        }
+    })
 })
